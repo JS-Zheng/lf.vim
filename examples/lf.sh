c1fadf98580a1105f4ca66d1a04ee0f2e19bea01
Jason Zheng <jszheng17@gmail.com>
2022-06-22 23:10:07 +0800

    chore: update lf.sh example

--- examples/lf.sh
+++ examples/lf.sh
@@ -1,25 +1,27 @@
 lf()
 {
-    local tmp="$(mktemp)"
-    # pass `LF_LAST_DIR` to a `lf` instance
-    # use `command` to avoid using alias
-    LF_LAST_DIR="$tmp" command lf "$@"
+    local tmp_dir="$(mktemp -d)"
+    trap 'command rm -rf -- '"$tmp_dir" EXIT
+
+    local lf_last_dir="$tmp_dir/lf_last_dir"
+
+    # Use `command` to avoid using alias
+    LF_LAST_DIR="$lf_last_dir" command lf "$@"
 
     # check if the file exists
-    if [[ ! -f "$tmp" ]]; then
+    if [[ ! -f "$lf_last_dir" ]]; then
         return
     fi
 
-    local dir="$(cat "$tmp")"
-    command rm -f "$tmp"
+    local dir="$(<"$lf_last_dir")"
 
-    # check if a valid directroy
+    # check if a valid directory
     if [[ ! -d "$dir" ]]; then
         return
     fi
 
     # change the working directory if necessary
-    if [[ "$dir" != "$(pwd)" ]]; then
+    if [[ "$dir" != "$PWD" ]]; then
         cd -- "$dir"
     fi
 }
